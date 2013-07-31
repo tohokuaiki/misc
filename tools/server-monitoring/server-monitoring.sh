cd52c1f7d5b249394dcb0a61a0a56127c47bff2a
ITOH Takashi <itoh@tohokuaiki.jp>
2013-07-31 20:32:41 +0900

    recovery sensor add.
    catchup recovery mode.
    in case of reboot, this will access again 5 minitues after.

--- tools/server-monitoring/server-monitoring.sh
+++ tools/server-monitoring/server-monitoring.sh
@@ -2,21 +2,32 @@
 <?php
 $now = date('YmdHis');
 $results = array();
+$recovery_results = array();
 $config_file = dirname(__FILE__).'/server-monitoring.ini';
 $config = parse_ini_file($config_file, true);
 $subject = sprintf('サーバ監視結果アラート(%s)', $config['config']['server_name']);
 $trouble_server_file = dirname(__FILE__).'/trouble_servers';
 $trouble_server_text = "";
+$trouble_server_text_new = "";
 $trouble_servers = array();
 if (file_exists($trouble_server_file)){
     $trouble_server_text = file_get_contents($trouble_server_file);
     $ts = explode("\n", $trouble_server_text);
-    foreach ($ts as $_t){
-        $_t = trim($_t);
-        if ($_t){
-            $_t = explode("\t", $_t);
+    foreach ($ts as $_ts_line){
+        $_ts_line = trim($_ts_line);
+        if ($_ts_line){
+            $_t = explode("\t", $_ts_line);
             if (count($_t) == 2){
-                $trouble_servers[] = $_t[0];
+                $now_unixtime    = strtotime($now);
+                $troble_unixtime = strtotime($_t[1]);
+                $has_been_stopped = $now_unixtime - $troble_unixtime > $config['config']['downtime_as_stop'];
+                $trouble_servers[$_t[0]] = array(
+                    'downtime' => $_t[1], 
+                    'has_been_stopped' => $has_been_stopped
+                    );
+                if ($has_been_stopped){
+                    $trouble_server_text_new .= $_ts_line."\n";
+                }
             }
         }
     }
@@ -36,9 +47,15 @@ $req->setConfig('ssl_verify_peer', false)->setConfig('ssl_verify_host', false)->
 
 foreach ($config['targetserver']['server'] as $url){
     $result = "";
+    $recovery_check_mode = false;
     $url_param = parse_url($url);
-    if (in_array($url_param['host'], $trouble_servers)){
-        continue;
+    if (in_array($url_param['host'], array_keys($trouble_servers))){
+        if ($trouble_servers[$url_param['host']]['has_been_stopped'] ){
+            continue;
+        }
+        else {
+            $recovery_check_mode = true;
+        }
     }
     $tmp_dir = sprintf('%s/tmp/%s', dirname(__FILE__), $url_param['host']);
     if (!file_exists($tmp_dir)){
@@ -65,6 +82,9 @@ foreach ($config['targetserver']['server'] as $url){
                     }
                 }
                 file_put_contents($tmp_dir.'/'.$now, $html);
+                if (!$result && $recovery_check_mode){
+                    $recovery_results[] = sprintf('[%s] is recovered.', $url_param['host']);
+                }
             }
             else {
                 $result .= "get no html...";
@@ -79,19 +99,23 @@ foreach ($config['targetserver']['server'] as $url){
         $result = sprintf("[%s]\n%s", $url_param['host'], $result);
         $results[] = $result;
         // add trouble-server-file
-        $trouble_server_text .= sprintf("%s\t%s\n", $url_param['host'], $now);
+        $trouble_server_text_new .= sprintf("%s\t%s\n", $url_param['host'], 
+                                            $recovery_check_mode ? $trouble_servers[$url_param['host']]['downtime']  :$now);
     }
 }
-file_put_contents($trouble_server_file, $trouble_server_text);
-if ($results){
+file_put_contents($trouble_server_file, $trouble_server_text_new);
+if ($results || $recovery_results){
     $header  = sprintf("From: %s\n", $config['config']['from']);
     $text = implode("\n", $results);
+    if ($recovery_results){
+        $text .= "\n\nRecovery status:\n".implode("\n", $recovery_results);
+    }
     mb_language("japanese");
     mb_internal_encoding("UTF-8");
     mb_send_mail($config['config']['checkmail'], $subject, $text, $header); 
 
 }
-var_dump($results); 
+var_dump($results, $recovery_results); 
 
 
 
