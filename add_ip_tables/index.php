e66cdc7777914ff811e1f08588b0eab1a0129971
ITOH Takashi <itoh@tohokuaiki.jp>
2016-07-13 17:38:48 +0900

    Create index.php

---
+++ add_ip_tables/index.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+  <title>iptables subscriber</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=2">
+</head>
+<body>
+<?php
+if (isset($_SERVER['AUTH_TYPE']) &&
+    isset($_SERVER["PHP_AUTH_USER"]) &&
+    isset($_SERVER["PHP_AUTH_PW"] )) {
+    require_once dirname(__FILE__).'/config.php';
+    $ip = $_SERVER['REMOTE_ADDR'];
+    $list = add_new_ip(get_registered_ip_list(), $ip);
+    printf('save your ip => %s [%s]<br /> <p>%s</p>',
+           save_ip_list($list) ? 'success': 'failed',
+           $ip, date('Y/m/d H:i:s'));
+}
+else {
+    echo "please set basic auth.";
+}
+?>
+</body>
+</html>
